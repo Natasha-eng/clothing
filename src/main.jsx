a50cdeb05cc7855b6edaabdad804274b5aaa54e1
Natasha-eng <natasha.shilova.89@gmail.com>
2023-06-28 19:23:18 +0300

    stripe

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './App';
-import { store, persistor } from './store/store';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import './index.scss';
+import { Elements } from "@stripe/react-stripe-js";
 
-const rootElement = document.getElementById('root');
+import App from "./App";
+import { store, persistor } from "./store/store";
+import { stripePromise } from "./utils/stripe/stripe.utils";
 
-render(
+import "./index.scss";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
-  </React.StrictMode>,
-  rootElement
+  </React.StrictMode>
 );
